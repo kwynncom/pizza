229241f1fd241bf51495d4dcf5a62109844efed9
kwynncom <buessresume@gmail.com>
2021-12-01 02:05:01 -0500

    client-side complete payment with PayPal

--- js/js10.js
+++ js/js10.js
@@ -1,5 +1,13 @@
-function total(e) {
-    const priceE = e.dataset.priceelement;
-    const total  = byid(priceE).innerHTML * e.value;
-    byid('total').innerHTML = total;
+function total() {
+    const qtye = byid('pepqty');
+    const total  = byid(qtye.dataset.priceelement).innerHTML * qtye.value;
+    if (!Number.isNaN(total)) byid('total').innerHTML = total;
+    if (total > 0.009) { byid('coPar').style.visibility = 'visible'; activatePPBtn(); }
+    else                 byid('coPar').style.visibility = 'hidden' ;
+
+}
+
+function payment_complete(details) {
+    byid('coPar').style.visibility = 'hidden' ;  
+    byid('orderOK').style.visibility = 'visible';
 }
---
+++ js/payPal10.js
@@ -0,0 +1,22 @@
+function activatePPBtn() {
+    
+    byid('paypal-button-container').innerHTML = '';
+    
+    [paypal.FUNDING.PAYPAL].forEach(function(fundingSource) {
+        const button = paypal.Buttons({ 
+            fundingSource: fundingSource,  
+            style: { height: 55 }, 
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{"amount" : {"currency_code" : "USD", "value" : byid('total').innerHTML }}]
+              });
+            },
+           onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                payment_complete(details.payer.name.given_name);
+            });
+            }
+        });
+        if (button.isEligible()) { button.render('#paypal-button-container'); }
+    });
+}
